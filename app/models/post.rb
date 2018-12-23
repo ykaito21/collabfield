c87e969a7a3b8d65c81b7346eaa2e675e353c630
Yuki Kaito <ykaito21@gmail.com>
2018-12-23 23:01:49 +0900

    Add validations to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :category_id, presence: true
+
   default_scope -> { includes(:user).order(created_at: :desc) }
   scope :by_category, -> (branch, category_name) do
     joins(:category).where(categories: {name: category_name, branch: branch})
@@ -14,3 +19,4 @@ class Post < ApplicationRecord
     where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
   end
 end
+
