62fa3825515ffba6f6b97425eef1f4a412da6c2f
Yuki Kaito <ykaito21@gmail.com>
2018-12-25 00:16:09 +0900

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversations - Define a private_conv_recipient helper method in the   private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model   and add specs for it - Create _heading.html.erb and _messages_list.html.erb files   inside the private/conversations/conversation - Define a load_private_messages in private/conversations_helper.rb   and add specs for it - Create a _new_message_form.html.erb inside the   private/conversations/conversation - Create a _open.js.erbinside private/conversations - Create a  position_and_visibility.js inside the   assets/javascripts/conversations - Create a  conversation_window.scss inside the   assets/stylesheets/partials - Define an opened_conversations_windows helper method in   ApplicationController - Create a _private_conversations_windows.html.erb inside the   layouts/application

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,4 +1,5 @@
 class Private::ConversationsController < ApplicationController
+
   def create
     recipient_id = Post.find(params[:post_id]).user.id
     conversation = Private::Conversation.new(sender_id: current_user.id,
@@ -7,6 +8,8 @@ class Private::ConversationsController < ApplicationController
       Private::Message.create(user_id: recipient_id,
                               conversation_id: conversation.id,
                               body: params[:message_body])
+
+      add_to_conversations unless already_added?
       respond_to do |format|
         format.js {render partial: 'posts/show/contact_user/message_form/success'}
       end
@@ -15,5 +18,16 @@ class Private::ConversationsController < ApplicationController
         format.js {render partial: 'posts/show/contact_user/message_form/fail'}
       end
     end
+  end
+
+  private
+
+  def add_to_conversations
+    session[:private_conversations] ||= []
+    session[:private_conversations] << @conversation.id
+  end
+
+  def already_added?
+    session[:private_conversations].include?(@conversation.id)
   end
 end
--- spec/models/private/conversation_spec.rb
+++ spec/models/private/conversation_spec.rb
@@ -1,9 +1,28 @@
-context 'Scopes' do
-  it 'gets a conversation between users' do
-    user1 = create(:user)
-    user2 = create(:user)
-    create(:private_conversation, recipient_id: user1.id, sender_id: user2.id)
-    conversation = Private::Conversation.between_users(user1.id, user2.id)
-    expect(conversation.count).to eq 1
+require 'rails_helper'
+
+RSpec.describe Private::Conversation, type: :model do
+  context 'Scopes' do
+    it 'gets a conversation between users' do
+      user1 = create(:user)
+      user2 = create(:user)
+      create(:private_conversation, recipient_id: user1.id, sender_id: user2.id)
+      conversation = Private::Conversation.between_users(user1.id, user2.id)
+      expect(conversation.count).to eq 1
+    end
+  end
+
+  context 'Methods' do
+    it 'gets an opposed user of the conversation' do
+      user1 = create(:user)
+      user2 = create(:user)
+      conversation = create(:private_conversation,
+                             recipient_id: user1.id,
+                             sender_id: user2.id)
+      opposed_user = conversation.opposed_user(user1)
+      expect(opposed_user).to eq user2
+    end
   end
 end
+
+
+
