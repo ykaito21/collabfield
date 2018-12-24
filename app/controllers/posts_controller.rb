e2163c9102ace5a850bd1e54d7cb76f86fa92de2
Yuki Kaito <ykaito21@gmail.com>
2018-12-24 19:23:46 +0900

    Inside a post add a form to contact a user - Define a contact_user_partial_path helper method in PostsHelper.   Add specs for the method - Create _contact_user.html.erb and _login_required.html.erb partials - Define a leave_message_partial_path helper method in PostsHelper.   Add specs for the method - Create _already_in_touch.html.erb and _message_form.html.erb   partial files - Define a @message_has_been_sent in PostsController's show action - Define a between_users scope inside the Private::Conversation model   Add specs for the scope - Define private_conversation and private_message factories - Define routes for Private::Conversations and Private::Messages - Define a create action inside the Private::Conversations - Create _success.js and _fail.js partials - Add feature specs to test the overall .contact-user form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
   before_action :redirect_if_not_signed_in, only: [:new]
   def show
     @post = Post.find(params[:id])
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
   end
 
   def hobby
@@ -54,6 +57,10 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:content, :title, :category_id)
                          .merge(user_id: current_user.id)
+  end
+
+  def conversation_exist?
+    Private::Conversation.between_users(current_user.id, @post.user.id).present?
   end
 
 
