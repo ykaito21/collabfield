291e01caf1ce997a4262aa5f0b3b70f2f3315aa9
Yuki Kaito <ykaito21@gmail.com>
2018-12-26 21:02:59 +0900

    add some code beforehand

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   include NavigationHelper
   include PostsHelper
   include Private::ConversationsHelper
+
+  def private_conversations_windows
+    params[:controller] != 'messengers' ? @private_conversations_windows : []
+  end
+
 end
